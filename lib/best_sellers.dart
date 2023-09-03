82bd1f3e3800bcef2a6bfc7278d7ae1bfecdd9c5
KingNedalord <azizbektolipov2006@gmail.com>
2023-09-03 16:08:30 +0500

    changed

--- lib/best_sellers.dart
+++ lib/best_sellers.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Main_Dishes extends StatefulWidget {
-  const Main_Dishes({super.key});
+class Best_Sellers extends StatefulWidget {
+  const Best_Sellers({super.key});
 
   @override
-  State<Main_Dishes> createState() => _Main_DishesState();
+  State<Best_Sellers> createState() => _Best_SellersState();
 }
 
-class _Main_DishesState extends State<Main_Dishes> {
+class _Best_SellersState extends State<Best_Sellers> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,7 +75,7 @@ class _Main_DishesState extends State<Main_Dishes> {
             flexibleSpace: Column(
               children: [
                 Text(
-                  "Main Dishes",
+                  "Best Sellers",
                   style: TextStyle(
                       color: Colors.white,
                       fontFamily: "EBG",
@@ -86,8 +86,8 @@ class _Main_DishesState extends State<Main_Dishes> {
                   height: MediaQuery.of(context).size.height * 0.02,
                 ),
                 Text(
-                    "Find the best selling dishes. All\n     "
-                    "meals are prepared fresh",
+                    "Everyone's favourite dishes and\n                       "
+                    "takeouts.",
                     style: TextStyle(
                         color: Colors.white, fontFamily: "EBG", fontSize: 17)),
                 SizedBox(
@@ -124,16 +124,16 @@ class _Main_DishesState extends State<Main_Dishes> {
                       child: SizedBox.fromSize(
                           size: Size.fromRadius(48), // Image radius
                           child: Image.asset(index == 0
-                              ? "assets/11.png"
+                              ? "assets/21.png"
                               : index == 1
-                                  ? "assets/12.png"
+                                  ? "assets/22.png"
                                   : index == 2
-                                      ? "assets/13.png"
+                                      ? "assets/24.png"
                                       : index == 3
                                           ? "assets/14.png"
                                           : index == 4
                                               ? "assets/15.png"
-                                              : "assets/16.png")),
+                                              : "assets/26.png")),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -142,16 +142,16 @@ class _Main_DishesState extends State<Main_Dishes> {
                           children: [
                             Text(
                               index == 0
-                                  ? "Fried Rice"
+                                  ? "Beef Burger"
                                   : index == 1
-                                      ? "Jollof Rice"
+                                      ? "Shawarma"
                                       : index == 2
-                                          ? "Pasta Rigatoni"
+                                          ? "Pizza Peperoni"
                                           : index == 3
-                                              ? "Pizza Peperoni"
+                                              ? "Cheesy Bread"
                                               : index == 4
                                                   ? "Amala"
-                                                  : "Butterfly Pasta",
+                                                  : "Jollof Spaghetti",
                               style: TextStyle(fontSize: 20),
                             ),
                             Container(
--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:warachow/best_sellers.dart';
 
 import 'main_dishes.dart';
 
@@ -20,7 +21,53 @@ class _Main_PageState extends State<Main_Page> {
         elevation: 0,
       ),
       drawer: Drawer(
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xFFFF785B),
+        elevation: 20,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Container(
+                    width: 50,
+                    height: 50,
+                    child: Image.asset("assets/profile.png")),
+                Text("Profile",
+                    style: TextStyle(fontSize: 25, color: Colors.white))
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                    width: 50,
+                    height: 50,
+                    child: Image.asset("assets/heart.png")),
+                Text("Wishlist",
+                    style: TextStyle(fontSize: 25, color: Colors.white))
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                    width: 50,
+                    height: 50,
+                    child: Image.asset("assets/medal.png")),
+                Text("Loyalty Points",
+                    style: TextStyle(fontSize: 25, color: Colors.white))
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                    width: 50,
+                    height: 50,
+                    child: Image.asset("assets/cart.png")),
+                Text("Payment Methods",
+                    style: TextStyle(fontSize: 25, color: Colors.white))
+              ],
+            ),
+          ],
+        ),
       ),
       body: CustomScrollView(
         slivers: [
@@ -71,24 +118,38 @@ class _Main_PageState extends State<Main_Page> {
             return Column(
               children: [
                 SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-                ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    child: Image.asset(
-                        index == 0 ? "assets/1.png" : "assets/2.png")),
                 MaterialButton(
-                  onPressed: () {Navigator.push(context, CupertinoPageRoute(builder: (_) => Main_Dishes()));},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        index == 0 ? "    Main Dishes" : "    Best Sellers",
-                        style: TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.w700),
-                      ),
-                      Icon(Icons.arrow_forward_ios_rounded)
-                    ],
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                            builder: (_) =>
+                                index == 0 ? Main_Dishes() : Best_Sellers()));
+                  },
+                  child: Container(
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            child: Image.asset(
+                                index == 0 ? "assets/1.png" : "assets/2.png")),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              index == 0
+                                  ? "    Main Dishes"
+                                  : "    Best Sellers",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.w700),
+                            ),
+                            Icon(Icons.arrow_forward_ios_rounded)
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                )
+                ),
               ],
             );
           }, childCount: 2))
